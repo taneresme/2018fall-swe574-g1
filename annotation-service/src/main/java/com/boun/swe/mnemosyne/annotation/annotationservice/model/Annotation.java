ecd2e6ca7b96349e931c9bb77abd87451b619244
Vedat Akses <vedatakses@gmail.com>
2018-12-25 23:32:22 +0300

    remove unused import

--- annotation-service/src/main/java/com/boun/swe/mnemosyne/annotation/annotationservice/model/Annotation.java
+++ annotation-service/src/main/java/com/boun/swe/mnemosyne/annotation/annotationservice/model/Annotation.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.sun.xml.internal.ws.client.sei.ResponseBuilder;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
